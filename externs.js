e1514057efd0cdfd7c663af11f68254dbe12ea58
hzhu <hz@therealhenry.com>
2015-04-30 20:07:22 -0700

    initial commit

---
+++ externs.js
@@ -0,0 +1,3 @@
+var window = {};
+  window.env = function () {
+};
---
+++ src/main/core.cljs
@@ -0,0 +1,15 @@
+(ns main.core
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :as ajax]))
+
+(enable-console-print!)
+
+(defn app-view []
+    (println "**********")
+    (println (^:export .-env js/window))
+    (println "**********")
+	[:div.sample "hello world"])
+
+(reagent/render-component [app-view] (.getElementById js/document "app"))
+
+
